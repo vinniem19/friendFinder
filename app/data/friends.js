fe8ed23d460cdf9eefe9e8e7f7806cee1fd3fdb5
Vinnie <cuzvin2@gmail.com>
2019-05-20 14:59:44 -0400

    routing is correct and friends data is complete.

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friends = [
+    {"name": "Alex", "img": "images/Alex.png", "scores" : [5, 4, 3, 2, 1, 4, 3, 2, 1, 5]},
+    {"name": "Cleopatra", "img": "images/Cleopatra.png", "scores" : [4, 3, 2, 1, 2, 3, 4, 5, 4, 3]},
+    {"name": "Donald", "img": "images/Donald.png", "scores" : [3, 2, 1, 2, 3, 4, 5, 4, 3, 2]},
+    {"name": "Jessica", "img": "images/Jessica.png", "scores" : [2, 1, 2, 3, 4, 5, 4, 3, 2, 1]},
+    {"name": "Jose", "img": "images/Jose.png", "scores" : [1, 2, 3, 4, 5, 4, 3, 2, 1, 2]},
+    {"name": "Leonardo", "img": "images/Leonardo.png", "scores" : [2, 3, 4, 5, 4, 3, 2, 1, 2, 3]},
+    {"name": "Mario", "img": "images/Mario.png", "scores" : [2, 1, 2, 3, 4, 5, 4, 3, 2, 1]},
+    {"name": "Roger", "img": "images/Roger.png", "scores" : [1, 2, 3, 4, 5, 4, 3, 2, 1, 2]},
+    {"name": "Tina", "img": "images/Tina.png", "scores" : [2, 3, 4, 5, 4, 3, 2, 1, 2, 3]},
+    {"name": "Wendy", "img": "images/Wendy.png", "scores" : [3, 4, 5, 4, 3, 2, 1, 2, 3, 4]},
+    {"name": "Alexandra", "img": "images/Alexandra.png", "scores" : [4, 5, 4, 3, 2, 1, 3, 4, 5, 4]},
+    {"name": "Devin", "img": "images/Devin.png", "scores" : [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]},
+    {"name": "Esteran", "img": "images/Estefan.png", "scores" : [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]},
+    {"name": "John", "img": "images/John.png", "scores" : [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]},
+    {"name": "Joselin", "img": "images/Joselin.png", "scores" : [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]},
+    {"name": "Liset", "img": "images/Liset.png", "scores" : [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]},
+    {"name": "Mary", "img": "images/Mary.png", "scores" : [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]},
+    {"name": "Sparkles", "img": "images/Sparkles.png", "scores" : [4, 2, 4, 2, 4, 2, 4, 2, 4, 2]},
+    {"name": "Vicky", "img": "images/Vicky.png", "scores" : [1, 5, 1, 5, 1, 5, 1, 5, 1, 5]}
+  ];
+
+module.exports = friends
+
