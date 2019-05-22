6744f15829e38c62a06177f0547dcd3baad9ca41
Vinnie <cuzvin2@gmail.com>
2019-05-22 15:37:14 -0400

    survey creates new friend, but there is errors I cannot understand

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,53 @@
-var friendData = require("../data/friends");
+var friends = require("../data/friends");
 
 module.exports = function(app) {
 
 app.get("/api/friends", function(req, res) {
-    res.json(friendData);
+    res.json(friends);
 });
 
 app.post("/api/friends", function(req, res) {
-    friendData.push(req.body);
+    friends.push(req.body);
     res.json(true);
-})
+
+    // Here, I set the variables and loop through newFriend array to begin my comparison
+            var bestMatch;
+            var possibleMatch = {};
+            var compare = 0;
+            // I want to loop through our friends array
+    for(var i = 0; i < friends.length; i++){
+        if (bestMatch === undefined) {
+            bestMatch = {};
+            bestMatch.name = i;
+            for (var h = 0; h < newFriend.scores.length; h++) {
+                compare += Math.abs(parseInt(friends[i].scores[h]) - parsInt(newFriend.scores[h]));
+            };
+            bestMatch.value = compare;
+
+            console.log("Possible One Value: ", bestMatch.value );
+            console.log("Possible One Name: ", friends[bestMatch.name] );
+            compare = 0;
+
+            i++;
+        };
+        possibleMatch.name = h;
+        console.log("friends indes2: ", h);
+
+        for (var i = 0; i < newFriend.scores.length; i++) {
+            compare += Math.abs(parseInt(friends[i].scores[h]) - parsInt(newFriend.scores[h]));
+        };
+        possibleMatch.value = compare;
+        compare = 0;
+        console.log("possibleMatch Value: ", possibleMatch.value);
+        console.log("possibleMatch ", friends[possibleMatch.name]);
+        if (bestMatch.value < possibleMatch.value) {
+            possibleMatch = {};
+        } else {
+            bestMatch = possibleMatch;
+            possibleMatch = {};
+        };
+        };
+});
+
 
 }
